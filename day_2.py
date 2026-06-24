e83bacd83a574a533c386753baf6799bf47f3cb5
Nusaiba Ahmed <nususc19@gmail.com>
2026-06-25 02:31:59 +0600

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,9 @@
+input = input("Enter a number: ")
+
+if input == "1":
+    print("You entered one.")
+else:
+    print("You did not enter one.")
+
+
+#the code is too simple for someone like me but had to commit something to github. I will try to make it more complex in the future.
